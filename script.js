6ce46384edb312fb2c3c24a1e1f770d2c8c581d7
dylan <dylan.rohart@hotmail.fr>
2023-08-22 13:16:27 +0200

    work in progress

---
+++ script.js
@@ -0,0 +1,26 @@
+const animatedDivs = document.querySelectorAll('.animate-left, .animate-right');
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function handleScroll() {
+  animatedDivs.forEach((animatedDiv) => {
+    if (isElementInViewport(animatedDiv)) {
+      if (animatedDiv.classList.contains('animate-left')) {
+        animatedDiv.classList.add('animated-div-left');
+      } else if (animatedDiv.classList.contains('animate-right')) {
+        animatedDiv.classList.add('animated-div-right');
+      }
+    }
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+
